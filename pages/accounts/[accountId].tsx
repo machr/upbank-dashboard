7ffd57cc2d3faf34b502775352ba87a9681dbd70
Mark Powell <hi@powellmark.com>
2022-10-27 10:33:27 +1100

    uncomment broken page

--- pages/accounts/[accountId].tsx
+++ pages/accounts/[accountId].tsx
@@ -1,44 +1,44 @@
 
-// export const getStaticPaths = async () => {
+// // export const getStaticPaths = async () => {
 
+// // }
+
+// export const getStaticProps = async (context) => {
+//   const accountId = context.params.id;
+//   const res = await fetch(`https://api.up.com.au/api/v1/accounts/${accountId}`, {
+//     headers: {
+//       "Content-type": "application/json;charset=UTF-8",
+//       Authorization: `Bearer ${process.env.UP_API_KEY}`
+//     },
+//   });
+//   const data = await res.json();
+
+//   const transactions = await fetch(`https://api.up.com.au/api/v1/accounts/${accountId}/transactions`, {
+//     headers: {
+//       "Content-type": "application/json;charset=UTF-8",
+//       Authorization: `Bearer ${process.env.UP_API_KEY}`
+//     },
+//   });
+//   const transactionsByAccount = await transactions.json();
+
+
+//   return {
+//     props: { account: data, transactionsByAccount: transactionsByAccount }
+//   }
 // }
 
-export const getStaticProps = async (context) => {
-  const accountId = context.params.id;
-  const res = await fetch(`https://api.up.com.au/api/v1/accounts/${accountId}`, {
-    headers: {
-      "Content-type": "application/json;charset=UTF-8",
-      Authorization: `Bearer ${process.env.UP_API_KEY}`
-    },
-  });
-  const data = await res.json();
+// const AccountPage = ({ account, transactionsByAccount }): JSX.Element => {
+//   // console.log('transactionsByAccount', transactionsByAccount);
 
-  const transactions = await fetch(`https://api.up.com.au/api/v1/accounts/${accountId}/transactions`, {
-    headers: {
-      "Content-type": "application/json;charset=UTF-8",
-      Authorization: `Bearer ${process.env.UP_API_KEY}`
-    },
-  });
-  const transactionsByAccount = await transactions.json();
+//   return (
+//     <>
+//       <h1>Account name: {account.data.attributes.displayName}</h1>
+//       <h2>Current Balance: ${account.data.attributes.balance.value}</h2>
+//       <main>
+//         <TransactionsList transactions={transactionsByAccount} />
+//       </main>
+//     </>
+//   )
+// }
 
-
-  return {
-    props: { account: data, transactionsByAccount: transactionsByAccount }
-  }
-}
-
-const AccountPage = ({ account, transactionsByAccount }): JSX.Element => {
-  console.log('transactionsByAccount', transactionsByAccount);
-
-  return (
-    <>
-      <h1>Account name: {account.data.attributes.displayName}</h1>
-      <h2>Current Balance: ${account.data.attributes.balance.value}</h2>
-      <main>
-        <TransactionsList transactions={transactionsByAccount} />
-      </main>
-    </>
-  )
-}
-
-export default AccountPage;
+// export default AccountPage;
